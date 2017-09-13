1559ffa89dfae2f25bb0669fef8eebc277d11973
SamuelLaaja <samuel.laaja@gmail.com>
2017-09-13 13:12:11 +0300

    Task 8

--- loop-statements/Task8/Program.cs
+++ loop-statements/Task8/Program.cs
@@ -6,7 +6,29 @@ namespace Task8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Random rand = new Random();
+            int randomNumber;
+
+            // Row loop from 1 to 4
+            for (int i = 1; i <= 4; i++)
+            {
+                // For each row, output 5 random numbers
+                Console.Write("Row " + i + ": ");
+                for (int j = 1; j <= 5; j++)
+                {
+                    // Random number is stored in r;
+                    randomNumber = rand.Next(50);
+                    // Ternary Conditional Operator ?: used for whitespace clean-up for numbers less than 10 and comma / newline switch
+                    Console.Write( (randomNumber > 10 ? "" : " ") + randomNumber + (j < 5 ? ", ": "\n") );
+
+                }
+
+            }
+            // Wait for user input to close program
+            Console.ReadKey();
+
         }
+
     }
 }
