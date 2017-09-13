fcd2b1d3a2acec85fa099d8c30aacfca5b190500
SamuelLaaja <samuel.laaja@gmail.com>
2017-09-13 12:23:42 +0300

    Tasks 6 & 7

---
+++ Loops/Task7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // From 1x1 to 9x9 output multiplication table with recursive for loops
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write(i + " x " + j + " = " + i*j + "   ");
+                }
+                Console.WriteLine();
+            }
+            // Wait for user input to close program
+            Console.ReadKey();
+
+        }
+
+    }
+}
